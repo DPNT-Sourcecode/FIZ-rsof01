e7bef11cd4b7231ddc90b624310832529c29394e
sbx_user1059 <sbx_user1059@ip-10-129-68-0.eu-west-2.compute.internal>
2018-11-28 22:33:16 +0000

    Wed Nov 28 22:33:16 UTC 2018

--- lib/send_command_to_server.py
+++ lib/send_command_to_server.py
@@ -63,5 +63,3 @@ ChallengeSession\
     .with_config(Utils.get_config())\
     .with_action_provider(lambda: get_user_input(sys.argv[1:]))\
     .start()
-
-
--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,5 +3,3 @@ def compute(x, y):
     return x+y
 
 #print(compute(2,3))    
-
-
