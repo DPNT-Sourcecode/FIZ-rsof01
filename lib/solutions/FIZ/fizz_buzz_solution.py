6344e7bc8608d54f351b9858389197a1bedda616
sbx_user1059 <sbx_user1059@ip-10-129-68-0.eu-west-2.compute.internal>
2018-11-28 22:53:49 +0000

    Wed Nov 28 22:53:49 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,3 +1,15 @@
 # noinspection PyUnusedLocal
 def fizz_buzz(number):
-    raise NotImplementedError()
+    if number%3==0:
+        print("Fizz")
+    
+    elif number%5==0:
+        print("Buzz")
+    
+    elif number%3==0 and number%5==0:
+        print("Fizz Buzz")
+    
+    else:
+        print(str(1))
+
+fizz_buzz(30)
