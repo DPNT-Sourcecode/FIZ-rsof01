24e47333e2988a6aaae64079423b724975133963
sbx_user1059 <sbx_user1059@ip-10-129-68-0.eu-west-2.compute.internal>
2018-11-28 22:41:18 +0000

    Wed Nov 28 22:41:18 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -3,9 +3,10 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
-    return("Hello, World!")
+    return("Hello, %s!" %friend_name)
 
 #print(hello("Mike"))
+
 
 
 
