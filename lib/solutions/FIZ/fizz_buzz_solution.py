b76e86c789730a35a97c4398862607fd30ec54e5
sbx_user1059 <sbx_user1059@ip-10-129-68-0.eu-west-2.compute.internal>
2018-11-28 23:14:29 +0000

    Wed Nov 28 23:14:29 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -6,11 +6,11 @@ def fizz_buzz(number):
     elif number%5==0:
         return("buzz")
     
-    elif number%3==0:
+    elif number%3==0 OR "3" in str(number):
         return("fizz")
     
     else:
         return(number)
 
-#fizz_buzz(1)
+fizz_buzz(1)
 
