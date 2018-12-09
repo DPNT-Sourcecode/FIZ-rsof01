4d3ef5922c8b0182ef2a1f97e95d43414bc1800f
sbx_user1059 <sbx_user1059@ip-10-129-2-9.eu-west-2.compute.internal>
2018-12-09 16:54:12 +0000

    Sun Dec 09 16:54:12 UTC 2018

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -4,12 +4,13 @@
 # friend_name = unicode string
 
 def hello(friend_name):
-    return("Hello, %s" %friend_name)
+    return("Hello, World")
 
 """
 def hello(friend_name):
     return("Hello, %s!" %friend_name)
 """
 #print(hello("Mike"))
+
 
 
