c4de1430fcb4267fdd6f7acdcfa4ee3c0f4daf53
sbx_user1059 <sbx_user1059@ip-10-129-2-9.eu-west-2.compute.internal>
2018-12-09 18:30:11 +0000

    Sun Dec 09 18:30:11 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -15,13 +15,22 @@ def samedigit(num):
 def fizz_buzz(number):
     if number > 10 and samedigit(number) == True and number%2!=0:
         return("fake deluxe")
+
+    elif number > 10 and samedigit(number) == True and (number%3==0 or "3" in str(number)) and  (number%5== 0 or "5" in str(number)) and number%2!=0:
+        return("fizz buzz fake deluxe")   
     
     elif number > 10 and samedigit(number) == True and (number%3==0 or "3" in str(number)) and  (number%5== 0 or "5" in str(number)):
         return("fizz buzz deluxe")
 
+    elif number > 10 and samedigit(number) == True and (number%5==0 or "5" in str(number)) and number%2!=0:
+        return("buzz fake deluxe")
+
     elif number > 10 and samedigit(number) == True and (number%5==0 or "5" in str(number)):
         return("buzz deluxe")
     
+    elif number > 10 and samedigit(number) == True and (number%3==0 or "3" in str(number)) and number%2!=0:
+        return("fizz fake deluxe")
+
     elif number > 10 and samedigit(number) == True and (number%3==0 or "3" in str(number)):
         return("fizz deluxe")
 
@@ -41,3 +50,4 @@ def fizz_buzz(number):
         return(number)
 
 #print(fizz_buzz(111))
+
