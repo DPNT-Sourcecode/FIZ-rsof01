1fa2a2b81a4335e49d5625b46f07f90a3bd88a2a
sbx_user1059 <sbx_user1059@ip-10-129-2-9.eu-west-2.compute.internal>
2018-12-09 17:33:20 +0000

    Sun Dec 09 17:33:20 UTC 2018

--- lib/solutions/FIZ/fizz_buzz_solution.py
+++ lib/solutions/FIZ/fizz_buzz_solution.py
@@ -1,6 +1,8 @@
 # noinspection PyUnusedLocal
-
+"""
 def fizz_buzz(number):
+    if number > 10 and ??
+    
     if (number%3==0 or "3" in str(number)) and (number%5== 0 or "5" in str(number)):
         return("fizz buzz") 
     
@@ -14,3 +16,10 @@ def fizz_buzz(number):
         return(number)
 
 print(fizz_buzz(30))
+"""
+
+s = "111"
+
+letters = list(s)
+
+print(letters.len)
